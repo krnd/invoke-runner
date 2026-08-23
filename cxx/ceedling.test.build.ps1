a0cca05ed0e78082f5d4d69a8894de09d0908130
krnd <krnd@users.noreply.github.com>
2026-08-23 13:06:04 +0200

    feat: add `ceedling:test:show` task

--- cxx/ceedling.test.build.ps1
+++ cxx/ceedling.test.build.ps1
@@ -1,5 +1,13 @@
-# ceedling.test.build.ps1 1.2
+# ceedling.test.build.ps1 1.3
 #Requires -Version 5.1
+
+
+# ################################ CONFIGURATION ###############################
+
+CONFIGURE ceedling.test.output `
+    -Default $null
+CONFIGURE ceedling.test.report `
+    -Default "tests_report.html"
 
 
 # ################################ TASKS #######################################
@@ -10,4 +18,10 @@ TASK ceedling:test:all {
 
 TASK ceedling:test:clean {
     EXEC { ceedling --ruby-replacement clean }
+}
+
+TASK ceedling:test:show {
+    $BuildRoot = (CONF ceedling.test.output)
+    $ReportName = (CONF ceedling.test.report)
+    Start-Process (Join-Paths $BuildRoot "artifacts" "test" $ReportName)
 }
